5d1ec14e60f9dddf2bb9db5d361e29b94a4ae193
lcskrishna <lollachaitanya@gmail.com>
2018-07-31 11:19:59 -0700

    updated workspace and fixes to buildfailure

--- vx_nn/src/rnn_layer.cpp
+++ vx_nn/src/rnn_layer.cpp
@@ -44,10 +44,10 @@ struct RNNLayerLocalData
     miopenTensorDescriptor_t bias_desc;
     miopenTensorDescriptor_t output_desc;
     miopenRNNDescriptor_t rnnDesc;
-    vx_size seq_len = 0;
+    vx_size seq_len;
 };
 
-static vx_status VX_CALLBACK validateRNNLayer(vx_node node, const vx_reference parameters[], vx_int32 num, vx_meta_format metas[])
+static vx_status VX_CALLBACK validateRNNLayer(vx_node node, const vx_reference parameters[], vx_uint32 num, vx_meta_format metas[])
 {
     //check scalar type for seq_length and mode.
     vx_enum type;
@@ -83,14 +83,17 @@ static vx_status VX_CALLBACK validateRNNLayer(vx_node node, const vx_reference p
     }
 
     //output tensors.
-    ERROR_CHECK_STATUS(vxQueryTensor((vx_tensor) parameters[5], VX_TENSOR_DATA_TYPE, &type, sizeof(type)));
-    ERROR_CHECK_STATUS(vxQueryTensor((vx_tensor) parameters[5], VX_TENSOR_NUMBER_OF_DIMS, &num_dims, sizeof(num_dims)));
+    vx_size output_dims[4];
+    ERROR_CHECK_STATUS(vxQueryTensor((vx_tensor) parameters[6], VX_TENSOR_DATA_TYPE, &type, sizeof(type)));
+    ERROR_CHECK_STATUS(vxQueryTensor((vx_tensor) parameters[6], VX_TENSOR_NUMBER_OF_DIMS, &num_dims, sizeof(num_dims)));
     if(type != VX_TYPE_FLOAT32) return ERRMSG(VX_ERROR_INVALID_TYPE, "validate: output: #7 type=%d (must be VX_TYPE_FLOAT32)\n", type);
+    ERROR_CHECK_STATUS(vxQueryTensor((vx_tensor)parameters[6], VX_TENSOR_DIMS, output_dims, sizeof(output_dims)));
+
 
     //output tensor configuration.
     type = VX_TYPE_FLOAT32;
     num_dims = 4;
-    ERROR_CHECK_STATUS(vxSetMetaFormatAttribute(metas[4], VX_TENSOR_DATA_TYPE, &type, sizeof(type)));return ERRMSG(VX_ERROR_INVALID_TYPE, "validate: seqlen : #5 type = %d (must be VX_TYPE_FLOAT32) \n", type);
+    ERROR_CHECK_STATUS(vxSetMetaFormatAttribute(metas[4], VX_TENSOR_DATA_TYPE, &type, sizeof(type)));
     ERROR_CHECK_STATUS(vxSetMetaFormatAttribute(metas[4], VX_TENSOR_NUMBER_OF_DIMS, &num_dims, sizeof(num_dims)));
     ERROR_CHECK_STATUS(vxSetMetaFormatAttribute(metas[4], VX_TENSOR_DIMS, output_dims, sizeof(output_dims)));
 
@@ -99,7 +102,8 @@ static vx_status VX_CALLBACK validateRNNLayer(vx_node node, const vx_reference p
 
 static vx_status VX_CALLBACK processRNNLayer(vx_node node, const vx_reference * parameters, vx_uint32 num)
 {
-    LSTMLocalLayerData * data = NULL;
+
+    RNNLayerLocalData * data = NULL;
     ERROR_CHECK_STATUS(vxQueryNode(node, VX_NODE_LOCAL_DATA_PTR, &data, sizeof(data)));
 
     ERROR_CHECK_STATUS(vxQueryTensor((vx_tensor)parameters[0], VX_TENSOR_BUFFER_OPENCL, &data->input_mem, sizeof(data->input_mem)));
@@ -117,27 +121,30 @@ static vx_status VX_CALLBACK processRNNLayer(vx_node node, const vx_reference * 
                                                             nullptr, nullptr, nullptr, nullptr, data->workspace, data->workspace_size));
     }
 
+
     return VX_SUCCESS;
 }
 
-static vx_status VX_CALLBACK initializeRNNLayer(vx_node node, const vx_refernece * parameters, vx_uint32 num)
+static vx_status VX_CALLBACK initializeRNNLayer(vx_node node, const vx_reference *parameters, vx_uint32 num)
 {
+
     RNNLayerLocalData * data = NULL;
     memset(data, 0, sizeof(*data));
     ERROR_CHECK_STATUS(createGraphHandle(node, &data->handle));
 
     //sequence length.
     vx_size mode = 0;
-    ERROR_CHECK_STATUS(vxCopyScalar((vx_scalar) parameters[5], &data->seq_len, VX_READ_ONLY, VX_MEMORY_TYPE_HOST));return ERRMSG(VX_ERROR_INVALID_TYPE, "validate: seqlen : #5 type = %d (must be VX_TYPE_FLOAT32) \n", type);
+    data->seq_len = 0;
+    ERROR_CHECK_STATUS(vxCopyScalar((vx_scalar) parameters[5], &data->seq_len, VX_READ_ONLY, VX_MEMORY_TYPE_HOST));
     ERROR_CHECK_STATUS(vxCopyScalar((vx_scalar) parameters[4], &mode, VX_READ_ONLY, VX_MEMORY_TYPE_HOST));
 
     //input, weights, hidden and output dimensions.
-    vx_size input_dims[4] = {1,1,0, 0}, output_dims[2], weight_dims[4] = {1,1,0,0}, hidden_dims[3], bias_dims[1];
+    vx_size input_dims[4] = {1,1,0, 0}, output_dims[4] = {1,1,0,0}, weight_dims[4] = {1,1,0,0}, hidden_dims[3], bias_dims[1];
     ERROR_CHECK_STATUS(vxQueryTensor((vx_tensor) parameters[0], VX_TENSOR_DIMS, input_dims, sizeof(input_dims)));
     ERROR_CHECK_STATUS(vxQueryTensor((vx_tensor) parameters[1], VX_TENSOR_DIMS, weight_dims, sizeof(weight_dims)));
     if (parameters[2]) ERROR_CHECK_STATUS(vxQueryTensor((vx_tensor) parameters[2], VX_TENSOR_DIMS, hidden_dims, sizeof(hidden_dims)));
     if (parameters[3]) ERROR_CHECK_STATUS(vxQueryTensor((vx_tensor) parameters[3], VX_TENSOR_DIMS, bias_dims, sizeof(bias_dims)));
-    ERROR_CHECK_STATUS(vxQueryTensor((vx_tensor) paramters[5], VX_TENSOR_DIMS, output_dims, sizeof(output_dims)));
+    ERROR_CHECK_STATUS(vxQueryTensor((vx_tensor) parameters[5], VX_TENSOR_DIMS, output_dims, sizeof(output_dims)));
 
     //tensor descriptors.
     miopenTensorDescriptor_t input_desc;
@@ -151,19 +158,19 @@ static vx_status VX_CALLBACK initializeRNNLayer(vx_node node, const vx_refernece
 
     //set tensor descriptors.
     for (int i=0; i < data->input_desc_array.size(); i++) {
-        std::array<int, 2> in_lens = {{input_dims[2], input_dims[3]}};
+        std::array<int, 2> in_lens = {(vx_int32) input_dims[2], (vx_int32) input_dims[3]};
         ERROR_CHECK_MIOPEN_STATUS(miopenSetTensorDescriptor(data->input_desc_array[i], miopenFloat, 2, in_lens.data(), nullptr));
     }
 
     for (int i=0; i < data->output_desc_array.size(); i++) {
-        std::array<int, 2> out_lens = {{output_dims[0], output_dims[1]}};
+        std::array<int, 2> out_lens = { (vx_int32) output_dims[2], (vx_int32) output_dims[3]};
         ERROR_CHECK_MIOPEN_STATUS(miopenSetTensorDescriptor(data->output_desc_array[i], miopenFloat, 2, out_lens.data(), nullptr));
     }
 
-    if (parameters[2]) {miopenLSTM
-        hidden_l = data->seq_len;
-        hidden_h = input_dims[2];
-        std::array<int, 3> hidden_lens = {{hidden_l , input_dims[2], hidden_h}};
+    if (parameters[2]) {
+        vx_size hidden_l =  data->seq_len;
+        vx_size hidden_h =  input_dims[2];
+        std::array<int, 3> hidden_lens = {(vx_int32) hidden_l , (vx_int32) input_dims[2], (vx_int32) hidden_h};
         ERROR_CHECK_MIOPEN_STATUS(miopenSetTensorDescriptor(data->hidden_input_desc, miopenFloat, 3, hidden_lens.data(), nullptr));
     }
 
@@ -192,7 +199,23 @@ static vx_status VX_CALLBACK initializeRNNLayer(vx_node node, const vx_refernece
     }
 
     //set RNN descriptor.
-    ERROR_CHECK_MIOPEN_STATUS(miopenSetRNNDescriptor(data->rnnDesc, input_dims[2], seq_len, miopenRNNlinear, miopenRNNunidirection, rnn_mode, bias_mode, miopenRNNdefault, miopenFloat));
+    ERROR_CHECK_MIOPEN_STATUS(miopenSetRNNDescriptor(data->rnnDesc, input_dims[2], data->seq_len, miopenRNNlinear, miopenRNNunidirection, rnn_mode, bias_mode, miopenRNNdefault, miopenFloat));
+
+    //workspace.
+    ERROR_CHECK_MIOPEN_STATUS(miopenGetRNNWorkspaceSize(data->handle->miopen_handle, data->rnnDesc, data->seq_len, data->input_desc_array.data(), &data->workspace_size));
+    if (data->workspace_size > 0) {
+        vx_context   vxContext = vxGetContext((vx_reference)node);
+        cl_context context;
+        ERROR_CHECK_STATUS(vxQueryContext(vxContext, VX_CONTEXT_ATTRIBUTE_AMD_OPENCL_CONTEXT, &context, sizeof(context)));
+        data->workspace_size = (data->workspace_size + 3) & ~3;
+        data->workspace = clCreateBuffer(context, CL_MEM_READ_WRITE, data->workspace_size, NULL, NULL);
+        if (!data->workspace) {
+            return VX_FAILURE;
+        }
+        cl_float pattern= 0;
+        cl_int err = clEnqueueFillBuffer(data->handle->cmdq, data->workspace, &pattern, sizeof(cl_float), 0, data->workspace_size, 0, NULL, NULL);
+        if(err) return VX_FAILURE;
+    }
 
     //create buffers.
     ERROR_CHECK_STATUS(vxQueryTensor((vx_tensor)parameters[0], VX_TENSOR_BUFFER_OPENCL, &data->input_mem, sizeof(data->input_mem)));
@@ -203,6 +226,7 @@ static vx_status VX_CALLBACK initializeRNNLayer(vx_node node, const vx_refernece
 
     //add to node attribute.
     ERROR_CHECK_STATUS(vxSetNodeAttribute(node, VX_NODE_LOCAL_DATA_PTR, &data, sizeof(data)));
+
     return VX_SUCCESS;
 }
 
@@ -229,7 +253,7 @@ static vx_status VX_CALLBACK uninitializeRNNLayer(vx_node node, const vx_referen
 vx_status publishRNNLayer(vx_context context)
 {
     // add kernel to the context with callbacks
-    vx_kernel kernel = vxAddUserKernel(context, "com.amd.nn_extension.lstm_layer", VX_KERNEL_RNN_LAYER_AMD, processRNNLayer, 6, validateRNNLayer, initializeRNNLayer, uninitializeRNNLayer);
+    vx_kernel kernel = vxAddUserKernel(context, "com.amd.nn_extension.rnn_layer", VX_KERNEL_RNN_LAYER_AMD, processRNNLayer, 7, validateRNNLayer, initializeRNNLayer, uninitializeRNNLayer);
     ERROR_CHECK_OBJECT(kernel);
 
     // enable OpenCL buffer access since the kernel_f callback uses OpenCL buffers instead of host accessible buffers
@@ -243,7 +267,7 @@ vx_status publishRNNLayer(vx_context context)
     ERROR_CHECK_STATUS(vxAddParameterToKernel(kernel, 3, VX_INPUT, VX_TYPE_TENSOR, VX_PARAMETER_STATE_OPTIONAL));
     ERROR_CHECK_STATUS(vxAddParameterToKernel(kernel, 4, VX_INPUT, VX_TYPE_SCALAR, VX_PARAMETER_STATE_REQUIRED));
     ERROR_CHECK_STATUS(vxAddParameterToKernel(kernel, 5, VX_INPUT, VX_TYPE_SCALAR, VX_PARAMETER_STATE_REQUIRED));
-    ERROR_CHECK_STATUS(vxAddParameterToKernel(kernel, 6, VX_INPUT, VX_TYPE_TENSOR, VX_PARAMETER_STATE_REQUIRED));
+    ERROR_CHECK_STATUS(vxAddParameterToKernel(kernel, 6, VX_OUTPUT, VX_TYPE_TENSOR, VX_PARAMETER_STATE_REQUIRED));
 
     //finialize and release kernel object.
     ERROR_CHECK_STATUS(vxFinalizeKernel(kernel));
@@ -252,7 +276,8 @@ vx_status publishRNNLayer(vx_context context)
     return VX_SUCCESS;
 }
 
-VX_API_ENTRY vx_node VX_API_CALL vxRNNLayer(vx_graph graph, vx_tensor input, vx_tensor weights, vx_tensor hidden, vx_tensor bias, vx_size mode, vx_size sequence_length, vx_tensor output) {
+VX_API_ENTRY vx_node VX_API_CALL vxRNNLayer(vx_graph graph, vx_tensor input, vx_tensor weights, vx_tensor hidden, vx_tensor bias, vx_size mode, vx_size sequence_length, vx_tensor output)
+{
     vx_node node = NULL;
     vx_context context = vxGetContext((vx_reference) graph);
     if (vxGetStatus((vx_reference) context) == VX_SUCCESS) {
@@ -270,6 +295,7 @@ VX_API_ENTRY vx_node VX_API_CALL vxRNNLayer(vx_graph graph, vx_tensor input, vx_
             };
             node = createNode(graph, VX_KERNEL_RNN_LAYER_AMD, params, sizeof(params)/sizeof(params[0]));
             vxReleaseScalar(&seq_len);
+            vxReleaseScalar(&rnn_mode);
         }
     }
     return node;
